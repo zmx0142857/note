002f8481e90e8e9c02c1b671d78ab088a1d4cae4
zmx <892298182@qq.com>
2021-02-10 11:00:06 +0800

    plot options: keepRatio, xtick

--- js/plot.js
+++ js/plot.js
@@ -31,17 +31,18 @@ Plot.prototype.geometry = function(config) {
   this.ymin = getDefault(config.ymin, -1);
   this.xmax = getDefault(config.xmax, 5);
   this.ymax = getDefault(config.ymax, 5);
-  this.scale = Math.min(
-    (this.canvas.width-2*this.padding) / (this.xmax-this.xmin),
-    (this.canvas.height-2*this.padding) / (this.ymax-this.ymin)
-  );
+  this.xscale = (this.canvas.width-2*this.padding) / (this.xmax-this.xmin);
+  this.yscale = (this.canvas.height-2*this.padding) / (this.ymax-this.ymin);
+  if (config.keepRatio) {
+    this.xscale = this.yscale = Math.min(this.xscale, this.yscale);
+  }
   return this;
 }
 
 // 另一种方案是用 css transform: scaleY(-1); 实现上下翻转；
 // 问题是，文字也会倒转，这可以用 ctx.scale(1, -1) 补救
 Plot.prototype.transform = function(x, y) {
-  return [(x-this.xmin)*this.scale+this.padding, (this.ymax-y)*this.scale+this.padding];
+  return [(x-this.xmin)*this.xscale+this.padding, (this.ymax-y)*this.yscale+this.padding];
 }
 
 Plot.prototype.moveTo = function(x, y) {
@@ -72,7 +73,13 @@ Plot.prototype.clear = function() {
   return this;
 }
 
-Plot.prototype.axis = function() {
+Plot.prototype.axis = function(config) {
+  config = config || {};
+  config.xtick = config.xtick || 1;
+  config.ytick = config.ytick || 1;
+  config.xlabel = config.xlabel || config.xtick;
+  config.ylabel = config.ylabel || config.ytick;
+
   var origin = this.transform(0, 0);
   this.ctx.strokeStyle = 'black';
   // x 轴
@@ -85,19 +92,23 @@ Plot.prototype.axis = function() {
   this.ctx.moveTo(origin[0], 0);
   this.ctx.lineTo(origin[0], this.canvas.height);
   this.ctx.stroke();
+
+  function myceil(x, step) {
+    return Math.ceil(x / step) * step;
+  }
   // 刻度
-  for (var x = Math.ceil(this.xmin); x <= this.xmax; ++x) {
+  for (var x = myceil(this.xmin, config.xtick); x <= this.xmax; x += config.xtick) {
     if (x !== 0) {
-      this.line(x, 0, x, 4/this.scale);
+      this.line(x, 0, x, 4/this.yscale);
     }
   }
-  for (var y = Math.ceil(this.ymin); y <= this.ymax; ++y) {
+  for (var y = myceil(this.ymin, config.ytick); y <= this.ymax; y += config.ytick) {
     if (y !== 0) {
-      this.line(0, y, 4/this.scale, y);
+      this.line(0, y, 4/this.xscale, y);
     }
   }
   // 标签
-  for (var x = Math.ceil(this.xmin); x <= this.xmax; ++x) {
+  for (var x = myceil(this.xmin, config.xlabel); x <= this.xmax; x += config.xlabel) {
     this.ctx.save();
     if (x !== 0) {
       this.ctx.translate(-2, 12);
@@ -111,7 +122,7 @@ Plot.prototype.axis = function() {
 
   this.ctx.save();
   this.ctx.translate(6, 4);
-  for (var y = Math.ceil(this.ymin); y <= this.ymax; ++y) {
+  for (var y = myceil(this.ymin, config.ylabel); y <= this.ymax; y += config.ylabel) {
     if (y !== 0) {
       this.text(''+y, 0, y);
     }
@@ -122,8 +133,8 @@ Plot.prototype.axis = function() {
 
 // plot discretely
 Plot.prototype.discrete = function(f, config={}) {
-  var xmin = this.xmin - this.padding / this.scale;
-  var xmax = this.xmax + this.padding / this.scale;
+  var xmin = this.xmin - this.padding / this.xscale;
+  var xmax = this.xmax + this.padding / this.xscale;
   var x = xmin;
   var step = getDefault(config.step, this.step);
   this.ctx.strokeStyle = getDefault(config.color, this.color);
@@ -136,8 +147,8 @@ Plot.prototype.discrete = function(f, config={}) {
 
 // plot continuously
 Plot.prototype.plot = function(f, config={}) {
-  var xmin = this.xmin - this.padding / this.scale;
-  var xmax = this.xmax + this.padding / this.scale;
+  var xmin = this.xmin - this.padding / this.xscale;
+  var xmax = this.xmax + this.padding / this.xscale;
   var x = xmin, y = f(x);
   var step = getDefault(config.step, this.step);
   var continuity = getDefault(config.continuity, 100);
@@ -165,8 +176,8 @@ window.Plot = Plot;
 })();
 
 /*
-var plot = new Plot('canvas', {xmin: -4.5, ymin: -1, xmax: 4.5, ymax: 5})
-plot.axis()
+var plot = new Plot('canvas', {xmin: -10, ymin: -1, xmax: 10, ymax: 5})
+  plot.axis({xlabel: 3})
   .plot(Math.floor)
   .plot(x => x*x, {color: Plot.colors.yellow})
   .plot(Math.cos, {color: Plot.colors.green})
