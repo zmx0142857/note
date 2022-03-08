3a800095cf998bdcdea7213aab2208619675c25b
zmx0142857 <892298182@qq.com>
2022-03-08 17:35:44 +0800

    add number/7

--- js/toc.js
+++ js/toc.js
@@ -71,6 +71,7 @@ math: {
     {title:'二次剩余', src:'number/4.html', date:'2022-01-09'},
     {title:'不定方程', src:'number/5.html', date:'2022-02-11'},
     {title:'积性数论函数', src:'number/6.html'},
+    {title:'素数分布', src:'number/7.html', date:'2022-03-08'},
     {title:'代数数域和代数整数', src:'number/11.html', date:'2022-03-05'},
     {title:'理想的分解', src:'number/12.html', date:'2022-03-04'},
   ]
@@ -168,7 +169,7 @@ math: {
     {title:'三角形, 四边形', src:'geometry/3.html', date:'2022-02-14'},
     {title:'圆', src:'geometry/4.html'},
     {title:'抛物线', src:'geometry/5.html'},
-    {title:'射影几何', src:'geometry/10.html'},
+    {title:'射影几何', src:'geometry/10.html', date:'2022-03-06'},
   ]
 }, {
   title:'解析几何',
